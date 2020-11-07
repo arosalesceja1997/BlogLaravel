dce1024412b6823dd7bdb200360ea8668d832951
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-11-07 14:10:47 -0600

    Anthony - Blog con laravel

---
+++ resources/views/dashboard/post/create.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.master')
+@section('content')
+  @include('dashboard.partials.validation')
+  <form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+    <div class="form-group">
+      <label for="title">Titulo</label>
+      <input type="text" class="form-control" name="title" id="title" placeholder="Title">
+      @error('title')
+        <small class="text-danger">{{ $message }}</small>
+      @enderror
+    </div>
+    <div class="form-group">
+      <label for="url_clean">Url limpia</label>
+      <input type="text" class="form-control" name="url_clean" id="url_clean" placeholder="url clean">
+      @error('url_clean')
+        <small class="text-danger">{{ $message }}</small>
+      @enderror
+    </div>
+    <div class="form-group">
+      <label for="content">Contenido</label>
+      <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+      @error('content')
+        <small class="text-danger">{{ $message }}</small>
+      @enderror
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+@endsection
